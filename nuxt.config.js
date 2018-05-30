aa5963642f975a06f0c881725c825a02387e7164
Maxime Leclerc <m.leclerc@cicd.biz>
2018-05-30 09:40:26 +0200

    feat(vuetify): install vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,36 +3,51 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'portfolio',
+    title: "portfolio",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Maxime Leclerc - Full stack web developer' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Maxime Leclerc - Full stack web developer"
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
+  /*
+  ** Stylus import
+  */
+  css: ["~/assets/stylus/app.styl"],
+  /*
+   ** Plugins
+   */
+  plugins: ["~/plugins/vuetify.js"],
   /*
   ** Build configuration
   */
   build: {
     /*
+    ** Build vendor
+    */
+    vendor: ["~/plugins/vuetify.js"],
+    extractCSS: true,
+    /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
