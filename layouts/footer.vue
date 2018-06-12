afb141bf28dc45f5fa290aa695cfe643093fcfa8
Maxime Leclerc <m.leclerc@cicd.biz>
2018-06-12 11:55:24 +0200

    feat(layout): layout integration

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
+  <footer app class="main-footer">
+    <div class="footer-top">
+      <v-layout column align-center justify-center>
 
-  </div>
+      </v-layout>
+    </div>
+  </footer>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      icons: [
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin'
+      ]
+    })
+  }
+</script>
