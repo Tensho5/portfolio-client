a79db73c83a105c0280578aec5fb739b4819ced1
Maxime Leclerc <leclercmaxime14@gmail.com>
2018-06-15 16:53:42 +0200

    feat(nav): add skills to navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,6 +9,7 @@
             <ul>
               <li><a href="#home" class="scrollactive-item">{{ $t('links.home') }}</a></li>
               <li><a href="#about" class="scrollactive-item">{{ $t('links.about') }}</a></li>
+              <li><a href="#skills" class="scrollactive-item">{{ $t('links.skills') }}</a></li>
               <li><a href="#portfolio" class="scrollactive-item">{{ $t('links.portfolio') }}</a></li>
               <li><a href="#blog" class="scrollactive-item">{{ $t('links.blog') }}</a></li>
               <li><a href="#contact" class="scrollactive-item">{{ $t('links.contact') }}</a></li>
