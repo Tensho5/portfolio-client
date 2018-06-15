fb943074392217ecdfc903fb094de7bb2dcdbce2
Maxime Leclerc <leclercmaxime14@gmail.com>
2018-06-15 12:24:35 +0200

    feat(skills): add skills section w/ web languages

--- pages/index.vue
+++ pages/index.vue
@@ -52,8 +52,74 @@
     </section>
     <section id="skills" class="section-bd-top secondary-bg-color">
       <v-container class="section-common-space">
+        <v-layout row>
+          <v-flex class="ma-2" xs4>
+            <v-card>
+              <v-list three-line>
+                <template v-for="(item, index) in languages">
+                  <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
+                  <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
+                  <v-list-tile v-else :key="item.title" avatar>
+                    <v-list-tile-avatar tile>
+                      <img :src="item.avatar">
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                      <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                      <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </template>
+              </v-list>
+            </v-card>
+          </v-flex>
+          <v-flex class="ma-2" xs4>
+            <v-card>
+              <v-list three-line>
+                <template v-for="(item, index) in items">
+                  <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
+                  <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
+                  <v-list-tile v-else :key="item.title" avatar>
+                    <v-list-tile-avatar tile>
+                      <img :src="item.avatar">
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                      <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                      <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </template>
+              </v-list>
+            </v-card>
+          </v-flex>
+          <v-flex class="ma-2" xs4>
+            <v-card>
+              <v-list three-line>
+                <template v-for="(item, index) in items">
+                  <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
+                  <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
+                  <v-list-tile v-else :key="item.title" avatar>
+                    <v-list-tile-avatar tile>
+                      <img :src="item.avatar">
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                      <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                      <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </template>
+              </v-list>
+            </v-card>
+          </v-flex>
+        </v-layout>
         <h3 class="section-title">{{ $t('skills.title') }}</h3>
         <p>{{ $t('skills.introduction') }}</p>
+        <img src="/img/vue.png" width="70"/>
+
+        <img src="/img/laravel.png" width="70"/>
+        <img src="/img/angular.png" width="70"/>
+        <img src="/img/sass.svg" width="70"/>
+        <img src="/img/stylus.svg" width="70"/>
+        <img src="/img/webpack.png" width="70"/>
       </v-container>
     </section>
     <section id="blog">
@@ -89,8 +155,33 @@
         { src: "/img/banner.jpg", text: ["home.title", "home.introduction"] },
         { src: "/img/device.jpg", text: [] },
         { src: "/img/banner.jpg", text: ["home.welcome"] },
+      ],
+      languages: [
+        { header: 'Languages' },
+        { avatar: '/img/php.png', title: 'PHP5/7', subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
+        { divider: true, inset: true },
+        { avatar: '/img/javascript.png', title: 'JavaScript/TS/ES6', subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend." },
+        { divider: true, inset: true },
+        { avatar: '/img/html.png', title: 'HTML5/CSS3', subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?" },
+        { divider: true, inset: true },
+        { avatar: '/img/angular.png', title: 'MySQL', subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?" }
+      ],
+      items: [
+        { header: 'Framework front' },
+        { avatar: '/img/vue.png', title: 'Brunch this weekend?', subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
+        { divider: true, inset: true },
+        { avatar: '/img/stylus.svg', title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>', subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend." },
+        { divider: true, inset: true },
+        { avatar: '/img/sass.svg', title: 'Oui oui', subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?" },
+        { divider: true, inset: true },
+        { avatar: '/img/angular.png', title: 'Birthday gift', subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?" },
+        { divider: true, inset: true },
+        { avatar: '/static/doc-images/lists/5.jpg', title: 'Recipe to try', subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos." }
       ]
-    })
+    }),
+     mounted () {
+
+     }
   }
 </script>
 
